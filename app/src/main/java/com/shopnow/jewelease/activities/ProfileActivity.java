55fcdcd68e07e6f8905be39fc40f5129ec918e24
earlfunk21 <60378076+earlfunk21@users.noreply.github.com>
2023-05-20 18:57:46 +0800

    Adding logout user

--- app/src/main/java/com/shopnow/jewelease/activities/ProfileActivity.java
+++ app/src/main/java/com/shopnow/jewelease/activities/ProfileActivity.java
@@ -2,6 +2,7 @@ package com.shopnow.jewelease.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -38,6 +39,11 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     public void logoutUser(View view) {
-        userPrefs.
+        SharedPreferences.Editor userEditor = userPrefs.edit();
+        userEditor.clear();
+        userEditor.apply();
+        Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
